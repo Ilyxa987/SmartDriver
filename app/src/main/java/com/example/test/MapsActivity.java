1ecdf0c013793e714fd807208f4a9566e2aa86d4
MargaritaPoliakova <margarita.03@list.ru>
2021-04-10 15:39:22 +0800

    hello

--- app/src/main/java/com/example/test/MapsActivity.java
+++ app/src/main/java/com/example/test/MapsActivity.java
@@ -15,6 +15,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
 
+    // hello there
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
